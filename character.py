75633e49cd542d842821bc1ad40725b47ca12af8
MaximKunets <kunets.ma@phystech.edu>
2020-11-30 12:19:25 +0300

    raw version 2

--- character.py
+++ character.py
@@ -1,7 +1,31 @@
 from game_objects import Blocks, Character
 
-def break_block(pos, x, y):
-    if ()
+def break_block(pos, x, y, blocks):
+    for g in blocks:
+        if cross_product(g.x, g.y, g.x + g.a, g.y, pos[0], pos[1]) \
+                and cross_product(g.x + g.a, g.y, g.x + g.a, g.y + g.a, pos[0], pos[1]) \
+                and cross_product(g.x + g.a, g.y + g.a, g.x, g.y + g.a, pos[0], pos[1]) \
+                and cross_product(g.x, g.y + g.a, g.x, g.y, pos[0], pos[1]):
+            if g.type == 1 and length(pos, x, y, g.a):
+                return True
+            else:
+                return False
+    return False
+'''
+Функция можно ли сломать блок и есть ли он вообще в месте клика
+если g.type ==1 то блок сломать можно
+'''
+def length(pos,x,y,a):
+    if (abs(pos[0] - x)<a/2 and abs(pos[1] - y)<3/2*a) or (abs(pos[0] - x)<3*a/2 and abs(pos[1] - y)<a/2):
+        return True
+    else:
+        return False
+'''
+Функция проверяет, находится ли блок рядом с персонажем
+x,y - координаты персонажа
+a -длина стороны блока
+pos - координаты клика
+'''
 
 def destroy_block(pos):
 
@@ -24,8 +48,7 @@ def checking_step_capability(x, y, blocks):
                 and cross_product(g.x + g.a, g.y + g.a, g.x, g.y + g.a, x, y) \
                 and cross_product(g.x, g.y + g.a, g.x, g.y, x, y):
             return False
-        else:
-            return True
+    return True
 '''
 Функция проверяет, есть ли блок в направлении, куда хочет пойти игрок
 x,y - координаты, куда хочет попасть игрок
@@ -44,7 +67,7 @@ def what_to_do(event, blocks):
         if event.kiy == pygame.K_s and checking_step_capability(Character.x, Character.y + Character.vy, blocks):
             Character.y += Character.vy
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-        if break_block(event.pos, Character.x, Character.y):
+        if break_block(event.pos,Character.x, Character.y,  blocks):
             destroy_block(event.pos)
 '''
 Функция задаёт, что делает герой при нажатии на кнопки управления
